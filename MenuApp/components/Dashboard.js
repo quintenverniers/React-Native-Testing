845f59440c82ddcd89f466de3e29cd283f2bbd3b
Quinten Verniers <quinten@Quintens-MacBook-Air.local>
2018-05-08 17:12:41 +0200

    sportmoment showing + menuworking

--- MenuApp/components/Dashboard.js
+++ MenuApp/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, FlatList } from 'react-native';
 import Icon from 'react-native-ionicons';
 
 export default class Dashboard extends React.Component {
@@ -10,6 +10,13 @@ export default class Dashboard extends React.Component {
             <Icon name="home" />
         ),
     };
+
+    constructor() {
+        super();
+        this.state = {
+            sportMoments: [{"id" : "1", "moment" : {"sport": "Running", "time": "13:00-14:00" }},{"id" : "2", "moment" : {"sport": "Fitness", "time": "16:00-18:00" }}],
+        }
+    }
     render() {
         return (
             <View style={styles.container}>
@@ -21,7 +28,11 @@ export default class Dashboard extends React.Component {
                 <View style={styles.toolBar}>
                     <Icon name="menu" onPress={() => this.props.navigation.toggleDrawer()} style={styles.menuIcon} /><Text style={styles.toolbarText}>Dashboard</Text>
                 </View>
-                <Text>Dashboard screen!</Text>
+                <FlatList style={styles.list}
+                    data={this.state.sportMoments}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <View style={styles.sportItem}><Text style={styles.momentTitle}>{item.moment.sport}</Text><Text style={styles.momentTime}>{item.moment.time}</Text></View>}
+                />
             </View>
         );
     }
@@ -51,4 +62,23 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 30,
     },
+    sportItem: {
+        marginTop: 5,
+        marginLeft: 3,
+        marginRight: 3,
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingBottom: 2,
+        borderWidth: 1/2,
+        borderLeftColor: 'red',
+        borderLeftWidth: 5,
+    },
+    momentTitle: {
+        color: 'black',
+        fontSize: 25,
+    },
+    momentTime: {
+        fontSize: 15,
+    },
+
 });
