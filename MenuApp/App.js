d45003b66a8c440d399872270a3ee6f01b8b9c36
Quinten Verniers <quinten@Quintens-MacBook-Air.local>
2018-05-08 21:25:31 +0200

    Image in Menu drawer

--- MenuApp/App.js
+++ MenuApp/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { StackNavigator , DrawerNavigator} from 'react-navigation';
+import { StackNavigator, DrawerItems, DrawerNavigator } from 'react-navigation';
 import Icon from 'react-native-ionicons';
 import Dashboard from './components/Dashboard.js';
 import Profile from './components/Profile.js';
 import Planning from './components/Planning.js';
 import About from './components/About.js';
 
+const DrawerContent = (props) => (
+  <View>
+    <View
+      style={{
+        backgroundColor: '#f50057',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text style={{ color: 'white', fontSize: 30 }}>
+        Header
+      </Text>
+    </View>
+    <DrawerItems {...props} />
+  </View>
+)
+
 const AppNavigator = DrawerNavigator({
-  Dashboard: {screen: Dashboard},
-  Profile: {screen: Profile},
-  Planning: {screen: Planning},
-  About: {screen: About}
-})
+  Dashboard: { screen: Dashboard },
+  Profile: { screen: Profile },
+  Planning: { screen: Planning },
+  About: { screen: About }
+},
+  {
+    contentComponent: DrawerContent,
+  })
 
 export default class App extends React.Component {
   render() {
