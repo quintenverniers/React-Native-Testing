992ff24afe61b6f33da026d30a4afa3e1a33e55a
Quinten Verniers <quinten@Quintens-MacBook-Air.local>
2018-05-09 13:09:23 +0200

    text added to about page

--- MenuApp/components/About.js
+++ MenuApp/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Image } from 'react-native';
 import Icon from 'react-native-ionicons';
 
 export default class About extends React.Component {
@@ -21,7 +21,15 @@ export default class About extends React.Component {
                 <View style={styles.toolBar}>
                     <Icon name="menu" onPress={() => this.props.navigation.toggleDrawer()} style={styles.menuIcon} /><Text style={styles.toolbarText}>About</Text>
                 </View>
-                <Text>About screen!</Text>
+                <View style={styles.aboutWrapper}>
+                    <Text style={styles.aboutMainTitle}>About Simplex</Text>
+                    <Text style={styles.aboutSubTitle}>Our Mission</Text>
+                    <Text style={styles.aboutText}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</Text>
+                </View>
+                <View style={styles.aboutInfo}>
+                        <Text>Simplex APP ©2018</Text>
+                        <Text>Version: 0.0.1 (Beta)</Text>
+                    </View>
             </View>
         );
     }
@@ -50,5 +58,30 @@ const styles = StyleSheet.create({
         top: 5,
         color: 'white',
         fontSize: 30,
+    },
+    aboutWrapper: {
+
+    },
+    aboutMainTitle: {
+        fontSize: 40,
+        color: '#000',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    aboutSubTitle: {
+        fontSize: 25,
+        color: '#000',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    aboutText: {
+        fontSize: 15,
+        color: '#000',
+        textAlign: 'center',
+    },
+    aboutInfo: {
+        position: 'absolute',
+        bottom: 2,
+        left: 2,
     },
 });
