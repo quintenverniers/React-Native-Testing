9ddfca8c566bf3b82389760b56a41bc63deda423
Quinten Verniers <quinten@Quintens-MacBook-Air.local>
2018-05-09 11:50:13 +0200

    getting the id and incrementing it

--- MenuApp/components/Dashboard.js
+++ MenuApp/components/Dashboard.js
@@ -21,7 +21,10 @@ export default class Dashboard extends React.Component {
         // berekening in Helper
         var sportMoments = [{ "id": "1", "moment": { "sport": "Running", "time": "13:00-14:00" }}, { "id": "2", "moment": { "sport": "Fitness", "time": "16:00-18:00" }},{ "id": "3", "moment": { "sport": "Swimming", "time": "11:00-12:00" }}];        
         //{ "id": "4", "moment": { "sport": "Running", "time": "13:00-14:00" }}
-        sportMoments.push({ "id": "4", "moment": { "sport": "Running", "time": "08:00-12:00" }});
+        var sportMomentID = parseInt(sportMoments[sportMoments.length-1].id)+1;
+        sportMoments.push({ "id": sportMomentID, "moment": { "sport": "Running", "time": "08:00-12:00" }});
+        var sportMomentID = parseInt(sportMoments[sportMoments.length-1].id)+1;
+        sportMoments.push({ "id": sportMomentID, "moment": { "sport": "Swimming", "time": "11:30-13:00" }});
         
 
         return (
@@ -37,7 +40,7 @@ export default class Dashboard extends React.Component {
                 <FlatList style={styles.list}
                     data={sportMoments}
                     keyExtractor={item => item.id}
-                    renderItem={({ item }) => <View style={styles.sportItem}><View style={styles.sportItemIconWrapper}><Icon name="alarm" style={{color: '#000', fontSize: 40}} /></View><View><Text style={styles.momentTitle}>{item.moment.sport}</Text><Text style={styles.momentTime}>{item.moment.time}</Text></View></View>}
+                    renderItem={({ item }) => <View style={styles.sportItem}><View style={styles.sportItemIconWrapper}><Icon name="alarm" style={{color: '#000', fontSize: 40}} /></View><View><Text style={styles.momentTitle}>{item.moment.sport}</Text><Text style={styles.momentTime}>{item.moment.time} {item.id}</Text></View></View>}
                 />
             </View>
         );
