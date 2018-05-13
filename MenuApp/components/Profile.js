494631481deda8e3374b129d37e4031c8ae8d696
Quinten Verniers <quinten@Quintens-MacBook-Air.local>
2018-05-13 17:05:09 +0200

    profile page created

--- MenuApp/components/Profile.js
+++ MenuApp/components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, ScrollView, Picker, CheckBox, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-ionicons';
 
 export default class Profile extends React.Component {
@@ -10,6 +10,48 @@ export default class Profile extends React.Component {
             <Icon name="person" />
         ),
     };
+
+    constructor() {
+        super()
+        this.state = {
+            poolType: 'pool',
+            poolCategoryID: '4bf58dd8d48988d105941735',
+            gymType: 'gym',
+            gymCategoryID: '4bf58dd8d48988d176941735',
+            runningCheck: false,
+            swimmingCheck: false,
+            fitnessCheck: false,
+        };
+    }
+
+    RunningChange() {
+        this.setState({
+            runningCheck: !this.state.check1
+        })
+    }
+
+    swimmingChange() {
+        this.setState({
+            swimmingCheck: !this.state.check2
+        })
+    }
+
+    fitnessChange() {
+        this.setState({
+            fitnessCheck: !this.state.check3
+        })
+    }
+
+    chooseVenue(VenueType, VenueCategory){
+        alert("Venue chosen"+
+    "\nTypeOfVenue: "+VenueType+
+    "\nCategory: "+VenueCategory);
+    }
+
+    saveChanges(){
+        alert("Profile Changes pushed to database");
+    }
+
     render() {
         return (
             <View style={styles.container}>
@@ -21,7 +63,63 @@ export default class Profile extends React.Component {
                 <View style={styles.toolBar}>
                     <Icon name="menu" onPress={() => this.props.navigation.toggleDrawer()} style={styles.menuIcon} /><Text style={styles.toolbarText}>Profile</Text>
                 </View>
-                <Text>Profile screen!</Text>
+                <ScrollView>
+                    <View style={styles.UserImageContainer}>
+                        {/*<Image source={require('../images/kenzo.png')} style= {styles.UserImage}/>*/}
+                    </View>
+                    <View style={styles.UserDataContainer}>
+                        <Text style={styles.UserDataTitle}>Personal Info </Text>
+
+                        <Text style={styles.DataFieldText}>Naam </Text>
+                        <TextInput style={{ borderColor: 'gray', borderBottomWidth: 0 }} />
+
+                        <Text style={styles.DataFieldText}>Email </Text>
+                        <TextInput style={{ borderColor: 'gray', borderBottomWidth: 0 }} />
+
+                        <Text style={styles.DataFieldText}>Phone </Text>
+                        <TextInput style={{ borderColor: 'gray', borderBottomWidth: 0 }} />
+
+                        <Text style={styles.DataFieldText}>Sex </Text>
+                        <TextInput style={{ borderColor: 'gray', borderBottomWidth: 0 }} />
+
+                        <Text style={styles.Label}>Date of Birth </Text>
+                        <TextInput style={{ borderColor: 'gray', borderBottomWidth: 0 }} />
+
+                        <Text style={styles.UserDataTitle}>Workout Info </Text>
+
+                        <View style={{ paddingBottom: 20, borderColor: 'gray', borderBottomWidth: 1 }}>
+                            <Text style={styles.UserDataSubTitle}>Sports</Text>
+
+                            <View style={styles.Checkbox}>
+                                <CheckBox style={styles.Checkbox} value={this.state.runningCheck} onChange={() => this.RunningChange()} />
+                                <Text style={styles.CheckBoxLabel} > Running </Text>
+                            </View>
+
+                            <View style={styles.Checkbox}>
+                                <CheckBox style={styles.Checkbox} value={this.state.swimmingCheck} onChange={() => this.swimmingChange()} />
+                                <Text style={styles.CheckBoxLabel}> Swimming </Text>
+                            </View>
+
+                            <TouchableOpacity style={styles.ChooseVenueButton} onPress={() => this.chooseVenue(this.state.poolType,this.state.poolCategoryID)}>
+                                <Text style={styles.ChooseVenue}>Choose pool ></Text>
+                            </TouchableOpacity>
+
+                            <View style={styles.Checkbox}>
+                                <CheckBox style={styles.Checkbox} value={this.state.fitnessCheck} onChange={() => this.fitnessChange()} />
+                                <Text style={styles.CheckBoxLabel}> Fitness </Text>
+                            </View>
+
+                            <TouchableOpacity style={styles.ChooseVenueButton} onPress={() => this.chooseVenue(this.state.gymType,this.state.gymCategoryID)}>
+                                <Text style={styles.ChooseVenue}>Choose gym ></Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.SaveButtonView}>
+                            <TouchableOpacity style={styles.SaveButton} onPress={() => this.saveChanges()} >
+                                <Text style={styles.SaveButtonText}>SAVE</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
         );
     }
@@ -51,4 +149,76 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 30,
     },
+    UserImageContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    UserImage: {
+        marginTop: 20,
+        height: 175,
+        width: 175,    
+    },
+    UserDataContainer: {
+        padding: 15,
+    },
+    UserDataTitle: {
+        fontSize: 25,
+        marginBottom: 10,
+        marginTop: 10,
+    },
+    DataFieldText: {
+        marginTop: 10,
+        fontSize: 15,
+    },
+    UserDataSubTitle: {
+        marginTop: 10,
+        fontSize: 15,
+    },
+    CheckBoxLabel: {
+        marginTop: 12,
+        flexDirection: 'row',
+        padding: 3,
+        fontSize: 15,
+    },
+    Checkbox: {
+        marginTop: 10,
+        flexDirection: 'row',
+        marginBottom: 5,
+    },
+    ChooseVenue: {
+        fontSize: 15,
+        color: 'gray',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    ChooseVenueButton: {
+        borderRadius: 2,
+        borderWidth: 1,
+        borderColor: 'rgba(0,0,0,0.2)',
+        height: 25,
+        width: 125,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 6,
+    },
+    SaveButtonView: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    SaveButton: {
+        marginTop: 20,
+        borderWidth: 1,
+        borderColor: 'rgba(0,0,0,0.2)',
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 50,
+        width: 100,
+        backgroundColor: 'red',
+    },
+    SaveButtonText: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white'
+      },
 });
