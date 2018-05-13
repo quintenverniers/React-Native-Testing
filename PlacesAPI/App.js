b0dc72b6156189ba89154eab08770d69d15fbd2b
Quinten Verniers <quinten@Quintens-MacBook-Air.local>
2018-05-13 18:34:52 +0200

    venues from lat lon working

--- PlacesAPI/App.js
+++ PlacesAPI/App.js
@@ -27,36 +27,21 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      GymActive: 'black',
-      PoolActive: 'gray',
-      showGyms: true,
-      venueType: 'Gyms',
-      poolData: [],
-      pools: [],
-      pool: '',
-      poolCheckinCount: '',
-      poolCheckin: '',
-      gymData: [],
-      gyms: [],
-      gym: '',
-      gymCheckinCount: '',
-      gymCheckin: '',
-      hereNow: '',
       poolCategory: '4bf58dd8d48988d105941735',
       gymCategory: '4bf58dd8d48988d176941735',
       venueData: '',
       venues: '',
       venue: '',
-      venueCheckinCount:'',
-      venueCheckin:'',
+      venueCheckinCount: '',
+      venueCheckin: '',
       longitude: 0,
       latitude: 0,
 
     }
   }
 
-  getVenues(location,categoryID){
-    api.getVenues(location,categoryID).then((res) => {
+  getLocationVenues(lat, lon, categoryID) {
+    api.getLocationBasedVenues(lat, lon, categoryID).then((res) => {
       this.setState({
         venueData: res,
         venues: res.response.venues,
@@ -67,36 +52,8 @@ export default class App extends React.Component {
     });
   }
 
-  getLocationVenues(lat,lon,categoryID){
-    api.getLocationBasedVenues(lat,lon,categoryID).then((res) => {
-      this.setState({
-        venueData: res,
-        venues: res.response.venues,
-        venue: res.response.venues[1].name,
-        venueCheckinCount: res.response.venues[1].hereNow.count,
-        venueCheckin: res.response.venues[1].hereNow.summary
-      })
-    });
-  }
-
-  showGyms(location,categoryID){
-    this.setState({ 
-      showGyms: true, 
-      venueType: 'Gyms', 
-      GymActive: 'black', 
-      PoolActive: 'gray' 
-    });
-    this.getVenues(location, categoryID);
-  }
-
-  showLocationGyms(lat,lon,categoryID){
-    this.setState({ 
-      showGyms: true, 
-      venueType: 'Gyms', 
-      GymActive: 'black', 
-      PoolActive: 'gray' 
-    });
-    this.getLocationVenues(lat,lon, categoryID);
+  showLocationGyms(lat, lon, categoryID) {
+    this.getLocationVenues(lat, lon, categoryID);
   }
 
   componentDidMount() {
@@ -109,54 +66,13 @@ export default class App extends React.Component {
         latitude: lat,
         longitude: lon,
       })
-      this.showLocationGyms(lat,lon,'4bf58dd8d48988d176941735');
+      //how to pass categoryID from the profile screen?
+      //this.showLocationGyms(lat, lon, '4bf58dd8d48988d176941735');
+      this.showLocationGyms(lat, lon, '4bf58dd8d48988d176941735');
     }, (error) => alert(JSON.stringify(error)), { enableHighAccuracy: true })
-
-    //this.showGyms('Ghent','4bf58dd8d48988d176941735');
-
-    /*
-    api.getVenues('Ghent','4bf58dd8d48988d105941735').then((res) => {
-      this.setState({
-        poolData: res,
-        pools: res.response.venues,
-        pool: res.response.venues[1].name,
-        poolCheckinCount: res.response.venues[1].hereNow.count,
-        poolCheckin: res.response.venues[1].hereNow.summary
-      })
-    });*/
-    /*
-    api.getVenues('Ghent','4bf58dd8d48988d176941735').then((res) => {
-      this.setState({
-        gymData: res,
-        gyms: res.response.venues,
-        gym: res.response.venues[1].name,
-        gymCheckinCount: res.response.venues[1].hereNow.count,
-        gymCheckin: res.response.venues[1].hereNow.summary
-      })
-    });
-
-    api.getHereNow('575ef2e9498e19229bfc0df8').then((res) => {
-      this.setState({
-        hereNow: res.response.hereNow.count,
-      })
-    });*/
   };
 
-
-
   render() {
-    /*
-    const gymView =
-      <FlatList style={styles.list}
-        data={this.state.gyms}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => <View style={styles.Item}><Text style={styles.VenueTitle}>{item.name}</Text><Text>{item.location.address}, {item.location.postalCode} {item.location.city}</Text></View>}
-      />
-    const poolView = <FlatList style={styles.list}
-      data={this.state.pools}
-      keyExtractor={item => item.id}
-      renderItem={({ item }) => <View style={styles.Item}><Text style={styles.VenueTitle}>{item.name}</Text><Text>{item.location.address}, {item.location.postalCode} {item.location.city}</Text></View>}
-    />*/
     const venueView = <FlatList style={styles.list}
       data={this.state.venues}
       keyExtractor={item => item.id}
@@ -170,15 +86,13 @@ export default class App extends React.Component {
           hidden={false}
         />
         <View style={styles.toolbar}>
-          <Icon name='menu' style={styles.toolbarIcon}/>
+          <Icon name='menu' style={styles.toolbarIcon} />
           <Text style={styles.toolbarText}>Choose sportvenue</Text>
         </View>
         <View style={styles.container}>
-          <Text style={styles.venueType}>{this.state.venueType}</Text>
+          {/* How to pass gyms from profile */}
+          <Text style={styles.venueType}>Gyms</Text>
           {venueView}
-          <View style={styles.bottomNav}>
-            <TouchableOpacity style={styles.iconGymButton} onPress={() => this.showLocationGyms('51.1211132','3.914752599999929','4bf58dd8d48988d176941735')}><Icon name='stopwatch' style={[{color: this.state.GymActive}]}/></TouchableOpacity><TouchableOpacity style={styles.iconPoolButton} onPress={() => this.showLocationGyms('51.1211132','3.914752599999929','4bf58dd8d48988d105941735')}><Icon name='water' style={[{color: this.state.PoolActive}]}/></TouchableOpacity>
-          </View>
         </View>
       </View>
     );
@@ -201,7 +115,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     left: 12,
     top: 9,
-    color : 'white',
+    color: 'white',
   },
   toolbarText: {
     position: 'absolute',
