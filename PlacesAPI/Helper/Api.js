c44a2c51695505aaadb9a3f14021c89010b7b838
Quinten Verniers <quinten@Quintens-MacBook-Air.local>
2018-05-13 18:39:40 +0200

    radius added to gyms

--- PlacesAPI/Helper/Api.js
+++ PlacesAPI/Helper/Api.js
@@ -21,7 +21,7 @@ var api = {
         var url = '';
         //category = gyms: search in 10km range
         if (category == '4bf58dd8d48988d176941735') {
-            url = 'https://api.foursquare.com/v2/venues/search?ll=' + lat + ',' + lon + '&categoryId=' + category + '&oauth_token=SE2B5F5334D2ZCSRF5M3KRCPKWC5BR5SCRZATWWWMTX4ZI4P&v=20180422';
+            url = 'https://api.foursquare.com/v2/venues/search?ll=' + lat + ',' + lon + '&radius=10000&categoryId=' + category + '&oauth_token=SE2B5F5334D2ZCSRF5M3KRCPKWC5BR5SCRZATWWWMTX4ZI4P&v=20180422';
             //url = 'https://api.foursquare.com/v2/venues/search?near=Ghent,be&radius=10000&categoryId=4bf58dd8d48988d176941735&oauth_token=SE2B5F5334D2ZCSRF5M3KRCPKWC5BR5SCRZATWWWMTX4ZI4P&v=20180422';
         } else {
             url = 'https://api.foursquare.com/v2/venues/search?ll=' + lat + ',' + lon + '&categoryId=' + category + '&oauth_token=SE2B5F5334D2ZCSRF5M3KRCPKWC5BR5SCRZATWWWMTX4ZI4P&v=20180422';
